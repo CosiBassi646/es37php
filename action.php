f0997c4ae19f005d4dbf57d2fe18ef1135ca6410
CosiBassi646 <bassicosimo@gmail.com>
2025-03-06 12:46:31 +0100

    migliorie estetiche 1

--- action.php
+++ action.php
@@ -5,8 +5,11 @@
     <title>areoporti della nazione selezionata</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-    <h1 class="text-danger text-center mb-3">Ecco tutti gli Aereoporti della nazione selezionata:</h1>
+  <body class="p-3 mb-2 bg-dark text-white">
+    <?php
+      $nazione = $_GET['nazioni'];//recupero delle variabili dalla index
+    ?>
+    <h1 class="text-danger text-center mb-3">Ecco tutti gli Aereoporti della nazione: <?php echo $nazione?>:</h1>
     <div class="w-50 mx-auto my-auto text-center">
     <table class="table table-bordered">
       <tr>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>es37php</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="p-3 mb-2 bg-dark text-white">
   <h1 class="text-danger text-center">Seleziona la nazione della quale vuoi visualizzare gli aereoporti: </h1>
     <div class="w-25 text-center mx-auto my-auto">
         <form action="./action.php">
@@ -23,7 +23,16 @@
             <br>
             <button type="submit" class="btn btn-success">Invia</button>
         </form>
+        <br>
+        <h1 class="text-danger text-center mt-3">Cerca i voli i base al giorno: </h1>
+      <form action="./ricercaGiorni.php">
+        <input type="date" id="birthday" name="birthday">
+        <br>
+        <br>
+        <button type="submit" class="btn btn-success">Invia</button>
+      </form>
     </div>
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
